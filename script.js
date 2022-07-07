b792fd69564606ecedc315a002c9aed2dd6c9e08
Lev Mikheenko <justlooked@gmail.com>
2022-07-07 16:42:58 +1200

    V2

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 let tg = window.Telegram.WebApp;
 
-tg.ThemeParams.bg_color = "red";
+tg.expang();
+
+tg.MainButton.text = "Changed Text"; //изменяем текст кнопки 
+tg.MainButton.setText("Changed Text1"); //изменяем текст кнопки иначе
+tg.MainButton.textColor = "#F55353"; //изменяем цвет текста кнопки
+tg.MainButton.color = "#143F6B"; //изменяем цвет бэкграунда кнопки
+tg.MainButton.setParams({"color": "#143F6B"}); //так изменяются все параметры 
