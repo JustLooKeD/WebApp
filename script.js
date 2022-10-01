9d15d416d0aa5b2e00efb4e00c766e484976bc30
Lev Mikheenko <justlooked@gmail.com>
2022-10-02 11:30:05 +1200

    Changes to be committed: 	modified:   script.js 	new file:   webapp.code-workspace

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ let tg = window.Telegram.WebApp;
 
 tg.expand();
 
-tg.MainButton.text = "Changed Text"; //изменяем текст кнопки 
-tg.MainButton.setText("Changed Text1"); //изменяем текст кнопки иначе
+tg.MainButton.text = "Ебать"; //изменяем текст кнопки 
+tg.MainButton.setText("Ебать2"); //изменяем текст кнопки иначе
 tg.MainButton.textColor = "#F55353"; //изменяем цвет текста кнопки
 tg.MainButton.color = "#3296ff"; //изменяем цвет бэкграунда кнопки
 tg.MainButton.setParams({"color": "#143F6B"}); //так изменяются все параметры 
